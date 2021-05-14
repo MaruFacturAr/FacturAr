34bccb2f269fb839d6c1ed5afb4ec6996ad8d647
TomasAngeleri <tomi.angeleri94@gmail.com>
2021-05-14 20:11:54 -0300

    Create company

---
+++ src/main/java/com/facturar/app/repository/CompanyDao.java
@@ -0,0 +1,9 @@
+package com.facturar.app.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.facturar.app.entity.CompanyEntity;
+
+public interface CompanyDao extends JpaRepository<CompanyEntity, Long> {
+
+}
---
+++ src/main/java/com/facturar/app/service/CompanyService.java
@@ -0,0 +1,23 @@
+package com.facturar.app.service;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.facturar.app.entity.CompanyEntity;
+import com.facturar.app.entity.UserEntity;
+
+public interface CompanyService {
+	
+	public Iterable<CompanyEntity> findAll();
+	
+	public Page<CompanyEntity> findAll(Pageable pageable);
+	
+	public Optional<CompanyEntity> findById(Long id);
+	
+	public CompanyEntity save(CompanyEntity company);
+	
+	public void deleteById(Long id);
+	
+}
