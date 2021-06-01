4850d7b8a1d853b796cdfbe78cbc38bd8192f489
TomasAngeleri <44233271+TomasAngeleri@users.noreply.github.com>
2021-06-01 20:51:48 -0300

    Mapeo de clases y fix en companyDao

--- src/main/java/com/facturar/app/repository/CompanyDao.java
+++ src/main/java/com/facturar/app/repository/CompanyDao.java
@@ -1,9 +1,11 @@
 package com.facturar.app.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.facturar.app.entity.CompanyEntity;
 
+@Repository
 public interface CompanyDao extends JpaRepository<CompanyEntity, Long> {
 
     CompanyEntity findByuserId(Long userId);
